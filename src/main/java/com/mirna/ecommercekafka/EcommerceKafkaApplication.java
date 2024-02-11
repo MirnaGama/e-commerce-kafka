77c5b28fd9e69bf580b19de435c5b73be3543c9e
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-02-11 13:00:16 -0300

    [develop] Spring Initializr

---
+++ src/main/java/com/mirna/ecommercekafka/EcommerceKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.mirna.ecommercekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceKafkaApplication.class, args);
+	}
+
+}
